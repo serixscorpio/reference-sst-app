1fbfbe92d178b966a07206adc39d2d26c9d77b1f
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-10-28 15:07:47 +0800

    Initial commit

---
+++ services/core/sql.generated.ts
@@ -0,0 +1,18 @@
+import { ColumnType,  RawBuilder } from 'kysely';
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export type Timestamp = ColumnType<Date | RawBuilder, Date | string | RawBuilder, Date | string | RawBuilder>;
+
+export interface Article {
+  articleID: string;
+  created: Generated<Timestamp>;
+  title: string;
+  url: string;
+}
+
+export interface Database {
+  article: Article;
+}
